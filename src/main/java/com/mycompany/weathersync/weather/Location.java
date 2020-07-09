e1c260177ec11a99c37cfe1be843bdb6f930a2a0
neido <neido@localhost>
2020-07-09 02:27:56 -0300

    fixed typing mistake

---
+++ src/main/java/com/mycompany/weathersync/weather/Location.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.weathersync.weather;
+
+/**
+ *
+ * @author yaichao
+ */
+public class Location {
+    double latitude;
+    double longitude;
+    
+    public Location (double latitude,double longitude){
+        this.latitude=latitude;
+        this.longitude=longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
