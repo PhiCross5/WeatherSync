fcee15569e0bf9a8c1a63058597740e58ec5e06c
PhiCross5 <noEmail>
2020-07-29 14:03:08 -0300

    fix 'unsupported weather code' edge case on OpenWeather backend

--- src/main/java/weather/Provider_OpenWeather.java
+++ src/main/java/weather/Provider_OpenWeather.java
@@ -88,8 +88,9 @@ public class Provider_OpenWeather implements Provider {
 	    return WeatherStatus.CLEAR;
 	}
 	else{
-	    if((conditionCode/100)!=5 | (conditionCode/100)!=8){
-		System.out.print("ConditionCode not implemented yet: nº"+conditionCode);
+	    if((((int)conditionCode/100)!=5) 
+		    && (((int)conditionCode/100)!=8)){
+		System.out.println("ConditionCode not implemented yet: nº"+conditionCode);
 	    }
 	    return this.ConditionCodes_Coarse.get(conditionCode/100);
 	}
