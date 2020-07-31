e2c53dfb3f3f5e32dca18344ff3e118f57266a60
PhiCross5 <noEmail>
2020-07-31 01:11:39 -0300

    Added coordinates for some cities on OpenWeather demo entry point

--- src/main/java/run/OpenWeather_fetch.java
+++ src/main/java/run/OpenWeather_fetch.java
@@ -11,12 +11,25 @@ import weather.Provider;
 import weather.Provider_OpenWeather;
 import weather.WeatherUnavailableException;
 import confidential.SuperSecretData;
+import weather.Location;
 /**
  *
  * @author neido
  */
 public class OpenWeather_fetch {
+    
+    //a static inner class with coordinates for some cities.
+   static class places{
+	public static Location SAO_PAULO=new Location(-23.533773,-46.625290);
+	public static Location SALVADOR=new Location(-12.9711100,-38.5108300);
+	public static Location NEW_YORK=new Location( 40.7142700,-74.0059700);
+	public static Location TORONTO=new Location(43.7001100,-79.4163000);
+	
+   }
+    
+    
     public static void main(String[] args) {
+	
 	double latitude;
 	double longitude;
 	String apiKey;
@@ -40,7 +53,7 @@ public class OpenWeather_fetch {
 	
 	//dump WeatherLog and Source JSON
 	try{
-	    WeatherLog sauce=provider.getWeather();
+	    WeatherLog sauce=provider.getWeather(places.SALVADOR);
 	    String doubleCheck=provider.getJSON();
 	    System.out.println("**weather provided**");
 	    System.out.println("temperature:"+(sauce.getTemperature()-273.15)
