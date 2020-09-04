03dc46268cd43679e04d2e52159b71c57f5bbd91
PhiCross5 <noEmail>
2020-09-04 10:21:30 -0300

    Adding documentation for the API as JavaDoc comments.

--- src/main/java/weather/MinimalLog.java
+++ src/main/java/weather/MinimalLog.java
@@ -9,6 +9,10 @@ package weather;
  *
  * @author neido
  */
+
+/**MinimalLog is the least common denominator among encapsulated weather data
+ objects. It is simple to integrate, carries only the most used parameters
+  and has a consistent API.**/
 public class MinimalLog implements WeatherLog{
     WeatherStatus status;
     double temperature;
@@ -17,14 +21,20 @@ public class MinimalLog implements WeatherLog{
         status=st;
         temperature=tp;
     }
+        /**gets an object representing the weather in a simplified, descriptive way
+	 (e.g. raining, cloudy, clear skies...)**/
     public WeatherStatus getStatus(){
         return status;
     }
     
+    /**gets the temperature as a numerical value (units in Kelvin).
+     **/
     public double getTemperature(){
         return temperature;
     }
     
+    /** gets a string that textually summarizes the all weather information in
+     this log.**/
     public String toString(){
 	return "MinimalLog{\nstatus: "+status
 		+"\ntemperature: "+temperature+"\n}";
