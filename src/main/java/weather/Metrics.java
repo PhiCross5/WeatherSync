2368782229f7b05f9422f1da7019c687dcf9f7a3
PhiCross5 <noEmail>
2020-09-08 14:33:27 -0300

    javadoc for new 'Metrics' enumerations

---
+++ src/main/java/weather/Metrics.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package weather;
+
+/**
+ *
+ * @author neido
+ */
+
+/**
+Metrics enumerators for weather parameters.
+* checking for those at run-time allows for automatic unit conversion.
+**/
+public enum Metrics {
+    KELVIN, CELSIUS, FAHRENHEIT, PERCENT, MILLIMETERS, LUMEN, ATMOSPHERES, 
+    POUNDS_PER_SQUARE_INCH,INCHES_OF_MERCURY,KILOPASCAL
+}
